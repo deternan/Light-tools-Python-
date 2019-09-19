eeaaede86b2574caf51816ff5db1f0ce7481744d
DELTA\BARRY.KE <barry.ke@deltaww.com>
2019-09-19 11:46:04 +0800

    Sort function

---
+++ Sort/BubbleSort.py
@@ -0,0 +1,31 @@
+# coding=utf8
+'''
+Created on January 08, 2018    11:32 PM
+
+@author: Chao-Hsuan Ke
+'''
+
+import random;
+# Random 
+unsortData = random.sample(range(100), 10)
+
+print("Original Data:", unsortData)
+
+def bubble_sort(lst):    
+    j = 0
+    tmp = 0
+    for i in range(0,len(lst)-1,1):
+        j = i
+        for k in range(0,len(lst)-1,1):
+            if lst[j] < lst[k]:
+                j = k
+        
+                tmp = lst[i]
+                lst[i] = lst[j]
+                lst[j] = tmp
+                    
+    return lst
+    
+sortData = bubble_sort(unsortData)
+    
+print("Sort", sortData)
---
+++ Sort/Sort.py
@@ -0,0 +1,13 @@
+# coding=utf8
+'''
+Created on 2017�~10��13��
+
+@author: Chao-Hsuan Ke
+'''
+
+num_arry = [100,10,15,20]
+
+sort_arry = sorted(num_arry)
+
+print (num_arry)
+print (sort_arry)

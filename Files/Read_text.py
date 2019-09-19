0e07e42cc3cdeb3aa3630c2e43b22a88fadaced9
DELTA\BARRY.KE <barry.ke@deltaww.com>
2019-09-19 15:00:23 +0800

    read file (text)

---
+++ Files/Read_text.py
@@ -0,0 +1,21 @@
+# coding=utf8
+
+'''
+version: September 19, 2019 02:28 PM
+Last revision: September 19, 2019 02:58 PM
+  
+Author : Chao-Hsuan Ke
+
+''' 
+
+filePath = "";        # file path
+fileName = "";        # file name
+
+f = open(filePath + fileName, 'r')
+#print(f.read())                 # print all at once time
+#print(len(f.readlines()))      # all line number
+
+    
+for line in f.readlines():    
+    print(line)
+    

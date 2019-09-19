ba3c15fc9f9cf9ecf1945cc9016907b4c453fa6c
DELTA\BARRY.KE <barry.ke@deltaww.com>
2019-09-19 15:13:53 +0800

    read file list in folder

---
+++ Files/Readfoler.py
@@ -0,0 +1,20 @@
+# coding=utf8
+
+'''
+version: September 19, 2019 03:08 PM
+Last revision: September 19, 2019 03:08 PM
+  
+Author : Chao-Hsuan Ke
+
+''' 
+
+from os import listdir
+
+# file filder
+filePath = "";     
+
+files = listdir(filePath)
+
+for fileName in files:
+    print(fileName)
+

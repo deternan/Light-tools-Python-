cfa7beb69a3205ad5e5ca2a32e786b4db02ffc92
DELTA\BARRY.KE <barry.ke@deltaww.com>
2019-09-23 13:51:53 +0800

    file renames

---
+++ Files/Renames.py
@@ -0,0 +1,31 @@
+# coding=utf8
+
+'''
+version: September 23, 2019 01:27 PM
+Last revision: September 23, 2019 01:51 PM
+  
+Author : Chao-Hsuan Ke
+
+''' 
+
+import os
+from os import listdir
+from os.path import isfile, isdir, join
+
+# file folder
+filePath = "";        # folder name    
+
+files = listdir(filePath)
+
+count = 0;
+
+for fileName in files:
+  fullpath = join(filePath, fileName)  
+  if isfile(fullpath):
+    print("old name：", fileName)
+    os.rename(join(filePath, fileName), join(filePath, str(count))+".json")
+    count +=1
+  elif isdir(fullpath):
+    print("folder：", fileName)
+   
+

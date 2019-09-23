232f8dde7c42e86720493f444ed7266893a5b018
DELTA\BARRY.KE <barry.ke@deltaww.com>
2019-09-23 14:17:38 +0800

    file exist check

---
+++ Files/FileExists_check.py
@@ -0,0 +1,26 @@
+# coding=utf8
+
+'''
+version: September 23, 2019 02:04 PM
+Last revision: September 23, 2019 02:04 PM
+  
+Author : Chao-Hsuan Ke
+
+''' 
+
+import os
+from os import listdir
+from os.path import isfile, isdir, join
+
+# file folder
+filePath = "C:\\Users\\barry.ke\\Desktop\\test\\";        # folder name    
+
+files = listdir(filePath)
+
+for fileName in files:
+  fullpath = join(filePath, fileName)  
+  if isfile(fullpath):    
+    print(os.path.isfile(fullpath))
+  
+   
+

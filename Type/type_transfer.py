c557470a552089e150d52260628d430dfb77d380
DELTA\BARRY.KE <barry.ke@deltaww.com>
2019-10-02 18:33:36 +0800

    type transfer

---
+++ Type/type_transfer.py
@@ -0,0 +1,29 @@
+# coding=utf8
+
+'''
+version: October 02, 2019 05:28 PM
+Last revision: October 02, 2019 06:29 PM
+  
+Author : Chao-Hsuan Ke
+
+'''
+
+# Integer
+a = 100
+print(a)
+print(type(a))
+
+# Integer to float
+b = float(a) 
+print(b)
+print(type(b))
+
+# Integer to String
+b = str(a)
+print(b)
+print(type(b))
+
+# String to Integer
+c = int(b)
+print(c)
+print(type(c))

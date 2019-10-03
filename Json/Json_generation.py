d63c6f10faaa854037b5d7e30311ed4872d622a9
DELTA\BARRY.KE <barry.ke@deltaww.com>
2019-10-03 16:17:57 +0800

    add other type

--- Json/Json_generation.py
+++ Json/Json_generation.py
@@ -10,8 +10,12 @@ Author : Chao-Hsuan Ke
 
 import json
 
+# type 1
 data = {}
 data['key'] = 'value'
 json_data = json.dumps(data)
-
 print ('JSON: ', json_data)
+
+# type 2
+my_json_string = json.dumps({'key1': '123', 'key2': '456'})
+print(my_json_string)

dafe1890c0774770dbb6dd680667db7ec4b6b956
DELTA\BARRY.KE <barry.ke@deltaww.com>
2019-10-28 11:54:06 +0800

    Count DuplicatedList

---
+++ CountDuplicatedList.py
@@ -0,0 +1,15 @@
+# coding=utf8
+
+'''
+count of duplicated items in arraylist
+created: October 28, 2019 11:41 AM
+Last revision: October 28, 2019 11:41 AM
+  
+Author : Chao-Hsuan Ke
+'''
+
+from collections import Counter
+
+l = ["a","b","b"]
+
+print(Counter(l))

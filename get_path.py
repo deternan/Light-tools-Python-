b4fa5da1c3fcc7ffd019fbe6d7ac1d4d19594f55
DELTA\BARRY.KE <barry.ke@deltaww.com>
2019-10-30 16:22:44 +0800

    get current path

---
+++ get_path.py
@@ -0,0 +1,14 @@
+# coding=utf8
+'''
+ version: October 30, 2019 04:21 PM
+ Last updated: October 30, 2019 04:22 PM
+  
+ Author : Chao-Hsuan Ke
+ 
+'''
+
+import os
+
+pathStr = os.path.dirname(os.path.abspath(__file__))
+
+print(pathStr)

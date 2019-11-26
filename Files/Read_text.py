caf0a6f74445e251e5cd8172c6a15e960fd735ef
DELTA\BARRY.KE <barry.ke@deltaww.com>
2019-11-26 09:28:51 +0800

    add read function

--- Files/Read_text.py
+++ Files/Read_text.py
@@ -2,20 +2,25 @@
 
 '''
 version: September 19, 2019 02:28 PM
-Last revision: November 14, 2019 02:22 PM
+Last revision: November 26, 2019 09:22 AM
   
 Author : Chao-Hsuan Ke
 
 ''' 
 
-filePath = "";        # file path
-fileName = "";        # file name
+filePath = "C:\\Users\\barry.ke\\Desktop\\";        # file path
+fileName = "Song Recommendation.txt";        # file name
 
-f = open(filePath + fileName, 'r')
+f = open(filePath + fileName, 'r', encoding="utf-8")
 #print(f.read())                 # print all at once time
 #print(len(f.readlines()))      # all line number
 
-    
-for line in f:    
-    print(line)
-    
+# read each lines
+# for line in f:
+#     print(line)
+
+# read function
+data = f.read()
+print(len(data))
+
+f.close()

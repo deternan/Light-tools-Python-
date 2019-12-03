e906a5b470c5eb742b19cdd69ef87f53bd09394f
DELTA\BARRY.KE <barry.ke@deltaww.com>
2019-12-03 14:48:36 +0800

    value check

---
+++ Value_check.py
@@ -0,0 +1,25 @@
+# coding=utf8
+'''
+NaN_check
+
+version: December 03, 2019 02:02 PM
+Last revision: December 03, 2019 02:12 PM
+
+Author : Chao-Hsuan Ke
+
+'''
+
+import numpy as np
+
+aa = 18
+divided = 3
+
+
+
+if(divided!=0):
+    print(aa/divided)
+else:
+    print('divided: 0')
+
+#print(np.isnan(divided/aa))
+

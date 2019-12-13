5133ef06e5d4757f9f44ff5793b893fb44213bd6
DELTA\BARRY.KE <barry.ke@deltaww.com>
2019-12-13 09:40:21 +0800

    add set()

--- Testing/test.py
+++ Testing/test.py
@@ -1,10 +1,18 @@
 # coding=utf8
 
-import tensorflow
+'''
+ version: December 10, 2019 02:29 PM
+ Last revision: December 13, 2019 09:39 AM
 
-from keras import Input
+@author: Chao-Hsuan Ke
+'''
 
-input = Input(shape=(32, ))
+# shape
+# input = Input(shape=(32, ))
+# array = (10,)
+# print(array)
 
-array = (10,)
-print(array)
+# set (建立一個無序不重複元素集)
+list1=[1,2,3,4,5,4,3]
+s=set(list1)
+print(s)

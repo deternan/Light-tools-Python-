689364b6fb0ae9437bf590df8ec529254eaeeeb5
DELTA\BARRY.KE <barry.ke@deltaww.com>
2019-12-13 10:46:36 +0800

    testing

--- Testing/test.py
+++ Testing/test.py
@@ -2,7 +2,7 @@
 
 '''
  version: December 10, 2019 02:29 PM
- Last revision: December 13, 2019 09:39 AM
+ Last revision: December 13, 2019 10:36 AM
 
 @author: Chao-Hsuan Ke
 '''
@@ -13,6 +13,24 @@
 # print(array)
 
 # set (建立一個無序不重複元素集)
-list1=[1,2,3,4,5,4,3]
-s=set(list1)
-print(s)
+# list1=[1,2,3,4,5,4,3]
+# s=set(list1)
+# print(s)
+
+# dict
+dictUser = {'Name': 'Zara', 'Age': 20}
+print(dictUser['Name'])
+print(dictUser['Age'])
+
+dictUser['Note'] = 'Child'
+print(dictUser);
+
+for key in dictUser:
+    print(key, dictUser[key])
+
+# enumerate
+#lst = [1,2,3,4,5,6]
+#lst = ['今','天','天','氣','好']
+lst = ['E','L','S','A']
+for index,value in enumerate(lst):
+  print((index,value))

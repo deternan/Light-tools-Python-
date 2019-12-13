89b3107863225c9fdba2857c4940f6004037e46b
DELTA\BARRY.KE <barry.ke@deltaww.com>
2019-12-13 11:21:05 +0800

    add max function

--- Testing/test.py
+++ Testing/test.py
@@ -18,19 +18,28 @@
 # print(s)
 
 # dict
-dictUser = {'Name': 'Zara', 'Age': 20}
-print(dictUser['Name'])
-print(dictUser['Age'])
+# dictUser = {'Name': 'Zara', 'Age': 20}
+# print(dictUser['Name'])
+# print(dictUser['Age'])
+#
+# dictUser['Note'] = 'Child'
+# print(dictUser);
+#
+# for key in dictUser:
+#     print(key, dictUser[key])
 
-dictUser['Note'] = 'Child'
-print(dictUser);
-
-for key in dictUser:
-    print(key, dictUser[key])
-
-# enumerate
+# enumerate (多用於在for循環中得到計數，利用它可以同時獲得索引和值，即需要index和value值的時候可以使用)
 #lst = [1,2,3,4,5,6]
 #lst = ['今','天','天','氣','好']
-lst = ['E','L','S','A']
-for index,value in enumerate(lst):
-  print((index,value))
+# lst = ['E','L','S','A']
+# for index,value in enumerate(lst):
+#   print((index,value))
+
+# Max
+a = [-10, -9, -8, 1, 3, -4, 6]
+# type 1
+tmp = max(a, key=lambda x: x)
+print(tmp)
+# type 2
+tmp = max(a, key=lambda x: abs(x))
+print(tmp)

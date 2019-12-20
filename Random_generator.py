6c4e1b1875100d8bcd1e830420dc6ef4206d3d6c
DELTA\BARRY.KE <barry.ke@deltaww.com>
2019-12-20 09:37:57 +0800

    add non-duplication generation

--- Random_generator.py
+++ Random_generator.py
@@ -1,7 +1,7 @@
 # coding=utf8
 '''
  version: October 15, 2019 05:59 PM
- Last revision: October 19, 2019 05:59 PM
+ Last revision: December 20, 2019 09:32 AM
   
  Author : Chao-Hsuan Ke
  
@@ -11,10 +11,24 @@
  
 '''
 
+import random
 from random import randint
 
 min = 0;
-max = 3;
+max = 30;
 
-for i in range(1, 11):
-    print(randint(min,max))
+# for i in range(1, 11):
+#     print(randint(min,max))
+
+# non duplication
+
+total = 100
+li = [i for i in range(total)]
+res = []
+num = 30
+for i in range(num):
+    t = random.randint(i,total-1)
+    res.append(li[t])
+    li[t], li[i] = li[i], li[t]
+
+print(res)

4c5f7c2ccbc68deea12a51f04ba8114566425623
DELTA\BARRY.KE <barry.ke@deltaww.com>
2019-12-23 14:44:33 +0800

    add output format

--- Files/Output_numpy.py
+++ Files/Output_numpy.py
@@ -2,21 +2,25 @@
 
 '''
 version: December 23 2019, 02:10 PM
-Last revision: December 23 2019, 02:16 PM
+Last revision: December 23 2019, 02:43 PM
 
 Author : Chao-Hsuan Ke
 '''
 
 import numpy as np
 
-base_dir = ''
+base_dir = 'D:\\Phelps\\GitHub\\Python\\Light-tools\\Files\\'
 outputfile = 'txtfile.txt'
 
 myArr = np.arange(15).reshape(3, 5)
 print(myArr)
 
-aa = c = np.array( [ [1,2], [3,4] ], dtype= int)
+aa = np.array( [ [1,2], [3,4] ], dtype=int)
+np.around(aa, decimals=5)
 print(aa)
 
+array_x2 = np.random.randint(10, size=(3,4))
+print(array_x2)
+
 # save
-np.savetxt(base_dir + outputfile, aa)
+np.savetxt(base_dir + outputfile, array_x2, fmt='%1.3f')

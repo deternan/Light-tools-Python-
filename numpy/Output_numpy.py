1352cfc4bca127eb47dd290d7b7ab6ba4d4a3808
DELTA\BARRY.KE <barry.ke@deltaww.com>
2019-12-23 17:10:08 +0800

    change folder

---
+++ numpy/Output_numpy.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+'''
+version: December 23 2019, 02:10 PM
+Last revision: December 23 2019, 02:43 PM
+
+Author : Chao-Hsuan Ke
+'''
+
+import numpy as np
+
+base_dir = 'D:\\Phelps\\GitHub\\Python\\Light-tools\\Files\\'
+outputfile = 'txtfile.txt'
+
+myArr = np.arange(15).reshape(3, 5)
+print(myArr)
+
+aa = np.array( [ [1,2], [3,4] ], dtype=int)
+np.around(aa, decimals=5)
+print(aa)
+
+array_x2 = np.random.randint(10, size=(3,4))
+print(array_x2)
+
+# save
+np.savetxt(base_dir + outputfile, array_x2, fmt='%2.5f')

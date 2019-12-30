77527be2c3d1a9c0fb67851b1f45a80a28cf9033
DELTA\BARRY.KE <barry.ke@deltaww.com>
2019-12-30 16:33:20 +0800

    stsrem testing

---
+++ System/system.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+'''
+version: December 30 2019, 04:26 PM
+Last revision: December 30 2019, 04:26 PM
+
+Author : Chao-Hsuan Ke
+'''
+
+import sys
+
+print('Plase input your name: ')
+name = sys.stdin.readline()
+print('Hello ', name)
+

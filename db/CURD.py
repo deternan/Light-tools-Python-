c56d8a370e7cab8f97b276ad94f2a2716ba0baf0
DELTA\BARRY.KE <barry.ke@deltaww.com>
2020-03-24 14:31:08 +0800

    add query by ObjectId

--- db/CURD.py
+++ db/CURD.py
@@ -2,7 +2,7 @@
 
 '''
 version: March 24, 09:20 AM
-Last revision: March 24, 10:13 AM
+Last revision: March 24, 02:30 PM
 
 Author : Chao-Hsuan Ke
 '''
@@ -16,17 +16,14 @@ https://www.w3schools.com/python/python_mongodb_find.asp
 '''
 
 from pymongo import MongoClient, errors
+from bson.objectid import ObjectId
 
-ip = ''
-port = 8083
+ip = ''     ## db IP
+port = 8083             ## db PORT
 
 dbName = 'ncov2019'
 collectionName = 'news_cdc'
 
-# client = MongoClient('mongodb://'+ ip + ':' + str(port))
-# db = client.test_database
-# print(db)
-# client.close()
 
 try:
     # try to instantiate a client instance
@@ -46,7 +43,7 @@ if client != None:
     # the list_database_names() method returns a list of strings
     database_names = client.list_database_names()
     #print ("database_names() TYPE:", type(database_names))
-    print ("The client's list_database_names() method returned", len(database_names), "database names.")
+    #print ("The client's list_database_names() method returned", len(database_names), "database names.")
     # for i in database_names:
     #     print(i)
 
@@ -63,9 +60,25 @@ if db != None:
 '''
 get collection data
 '''
-mycollection = db[collectionName]
-if mycollection != None:
-    for x in mycollection.find():
-        print(x)
+collection = db[collectionName]
+# if collections != None:
+#     for x in collection.find():
+#         print(x)
 
 
+'''
+query by ObjectId
+'''
+def get(post_id):
+    print(ObjectId(post_id))
+    # Convert from string to ObjectId:
+    document = collection.find_one({'_id': ObjectId(post_id)})
+    print(document)
+
+
+idStr = '5e787132e2e8d50291462a8a'
+get(idStr)
+
+'''
+insert document
+'''

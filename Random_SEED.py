0207475c9f8c9e73ed262dd1ea74746fc8186ec1
DELTA\BARRY.KE <barry.ke@deltaww.com>
2020-04-22 19:47:04 +0800

    generated random number by SEED

--- Random_SEED.py
+++ Random_SEED.py
@@ -3,7 +3,7 @@
 '''
 Random (SEED)
 created: October 29, 2019 09:32 AM
-Last revision: October 29, 2019 09:34 AM
+Last revision: April 22, 2020 07:34 PM
   
 Author : Chao-Hsuan Ke
 '''
@@ -12,5 +12,9 @@ import random
 
 random.seed(618)        # setting Random SEED number
 
-for x in range(10):
-    print (random.randint(1,100))
+randomList = []
+ for x in range(len(list1)):
+   print (random.randint(1,100))
+
+
+

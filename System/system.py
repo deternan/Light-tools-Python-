e19aa88e4d6d07ddde8feae1b09b588afc737d75
DELTA\BARRY.KE <barry.ke@deltaww.com>
2020-04-22 11:27:13 +0800

    system info.

--- System/system.py
+++ System/system.py
@@ -9,7 +9,16 @@ Author : Chao-Hsuan Ke
 
 import sys
 
-print('Plase input your name: ')
-name = sys.stdin.readline()
-print('Hello ', name)
+# print('Plase input your name: ')
+# name = sys.stdin.readline()
+# print('Hello ', name)
+
+text = 'Synthetic opioids like fentanyl ravaged the US during the last decade. They may do even more damage to Asia in the next'
+for n, l in enumerate(text):
+    print(n, l)
+
+#print(len(temp))
+temp = text.split(' ')
+for i in temp:
+    print(i)
 

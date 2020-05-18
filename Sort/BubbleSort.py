52d852627c233a8280e8e6ffd83405d2c4ba18bf
DELTA\BARRY.KE <barry.ke@deltaww.com>
2020-05-18 15:15:01 +0800

    revised bubble sort and added unittest

--- Sort/BubbleSort.py
+++ Sort/BubbleSort.py
@@ -1,31 +1,56 @@
 # coding=utf8
 '''
 Created on January 08, 2018    11:32 PM
-
+Last revised : May 18, 2020 03:13 PM
 author: Chao-Hsuan Ke
 '''
 
 import random;
+import unittest
+
 # Random 
 unsortData = random.sample(range(100), 10)
+#print("Original Data:", unsortData)
 
-print("Original Data:", unsortData)
+def bubble_sort(sourceBubbleSortList):
+    listLength = len(sourceBubbleSortList)
+    i = 0
+    while i < listLength - 1:
+        j = listLength - 1
+        while j > i:
+            if sourceBubbleSortList[j] < sourceBubbleSortList[j - 1]:
+                tempNum = sourceBubbleSortList[j]
+                sourceBubbleSortList[j] = sourceBubbleSortList[j - 1]
+                sourceBubbleSortList[j - 1] = tempNum
+            j = j -1
+        i = i +1
 
-def bubble_sort(lst):    
-    j = 0
-    tmp = 0
-    for i in range(0,len(lst)-1,1):
-        j = i
-        for k in range(0,len(lst)-1,1):
-            if lst[j] < lst[k]:
-                j = k
-        
-                tmp = lst[i]
-                lst[i] = lst[j]
-                lst[j] = tmp
-                    
-    return lst
-    
-sortData = bubble_sort(unsortData)
-    
-print("Sort", sortData)
+    return sourceBubbleSortList
+
+
+class TestSortFunc(unittest.TestCase):
+    def test_bubble_sort_1(self):
+        arr = []
+        self.assertEquals([], bubble_sort(arr))
+
+    def test_bubble_sort_2(self):
+        arr = [618]
+        self.assertEquals([618], bubble_sort(arr))
+
+    def test_bubble_sort_3(self):
+        self.assertEquals(sorted(unsortData), bubble_sort(unsortData))
+
+
+
+if __name__ == '__main__':
+    # Unit Test
+    # suite
+    suite = unittest.TestSuite()
+    tests = [TestSortFunc("test_bubble_sort_1"), TestSortFunc("test_bubble_sort_2"), TestSortFunc("test_bubble_sort_3")]
+    suite.addTests(tests)
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite)
+
+    # Display
+    sortData = bubble_sort(unsortData)
+    print("Sort", sortData)

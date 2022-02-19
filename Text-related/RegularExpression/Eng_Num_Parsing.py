3ff48f05e71dab2ffabd4aa4bd49989a12ebaf88
deternan <chke@iir.csie.ncku.edu.tw>
2022-02-19 23:07:19 +0800

    parsing

--- Text-related/RegularExpression/Eng_Num_Parsing.py
+++ Text-related/RegularExpression/Eng_Num_Parsing.py
@@ -10,7 +10,10 @@ Author : Chao-Hsuan Ke
 
 import re
 
-patternStr = r"[a-zA-z]+"
-input_str = "Biden and b2 Ukrainian M3 President Zelensky will speak 1a today";
+#patternStr = r"[a-zA-z]+"
+input_str = "Biden and b2 Ukrainian M3 President 17m Zelensky will speak 1a today";
 capital = re.findall('[A-Za-z0-9]{2,4}', str(input_str))
+print(capital)
+
+capital = re.findall('[0-9]{1,2}\s?[A-Za-z]{1,5}', str(input_str))
 print(capital)

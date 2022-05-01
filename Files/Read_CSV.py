133a5425b11db651d53f3bfc2468c9c6955be4b9
chaohsuan.ko <chke@iir.csie.ncku.edu.tw>
2022-05-01 23:31:34 +0800

    read csv file

--- Files/Read_CSV.py
+++ Files/Read_CSV.py
@@ -2,7 +2,7 @@
 
 '''
 version: September 23, 2019 11:39 AM
-Last revision: January 20, 2020 05:10 PM
+Last revision: May 01, 2022 11:30 PM
   
 Author : Chao-Hsuan Ke
 
@@ -25,3 +25,11 @@ with open(csvfile, "r", encoding="utf-8") as f:
     myCsv = csv.reader(f)
     for row in myCsv:
         print(row)
+
+
+# 讀取 CSV 檔內容，將每一列轉成一個 dictionary
+with open(csvfile, newline='') as f:
+  rows = csv.DictReader(f)
+  # 以迴圈輸出指定欄位
+  for row in rows:
+    print(row['name'], row['id'], row['extension'], row['phone'])

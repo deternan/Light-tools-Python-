762ff3e825f01690aa598d329e3c28cf234b23ae
chaohsuan.ko <chke@iir.csie.ncku.edu.tw>
2022-05-01 19:31:33 +0800

    Permutation

---
+++ Permutation.py
@@ -0,0 +1,10 @@
+# coding=utf8
+'''
+created: May 01, 2022 07:24 PM
+Last updated: May 01, 2022 07:24 PM
+
+Author : Chao-Hsuan Ke
+'''
+
+from itertools import product
+print(list(product('ABC', 'xyz')))
